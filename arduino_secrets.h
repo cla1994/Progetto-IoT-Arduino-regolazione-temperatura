8a3901ddd989f8e2039028dd14901a69d2527850
cla1994 <116500326+cla1994@users.noreply.github.com>
2024-07-11 14:44:28 +0200

    Create arduino_secrets.h

---
+++ arduino_secrets.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID "Rete domestica"            // propri dati personali, nome e password del network
+#define SECRET_PASS "Password di rete"         // file aggiuntivo per garantire la sicurezza delle credenziali
